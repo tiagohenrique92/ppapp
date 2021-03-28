27b069f64898d5b1c5deedb95ccf97522fac1875
Tiago Henrique <h8.tiago@gmail.com>
2021-03-28 12:03:46 -0300

    Adicionado mais definições ao container;

--- app/container.php
+++ app/container.php
@@ -4,7 +4,6 @@ namespace PPApp;
 use DI\Container;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
-use PPApp\Models\TransactionModel;
 use PPApp\Models\UserModel;
 use PPApp\Models\WalletModel;
 use PPApp\Repositories\TransactionRepository;
@@ -18,35 +17,43 @@ use PPApp\Services\WalletService;
 use Psr\Container\ContainerInterface;
 
 $container = new Container();
-$container->set("externalAuthorizationService", function () {
+$container->set(ExternalAuthorizationService::class, function () {
     $url = "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6";
     return new ExternalAuthorizationService($url);
 });
-$container->set("externalNotificationService", function () {
+$container->set(ExternalNotificationService::class, function () {
     $url = "https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04";
     return new ExternalNotificationService($url);
 });
-$container->set("logger", function () {
+$container->set(Logger::class, function () {
     $logger = new Logger('ppapp_logger');
     $file_handler = new StreamHandler(__DIR__ . '/../logs/app.log');
     $logger->pushHandler($file_handler);
     return $logger;
 });
-$container->set(UserService::class, function () {
-    $userService = new UserService(new UserRepository(new UserModel()));
+$container->set(PaymentService::class, function (ContainerInterface $container) {
+    $logger = $container->get(Logger::class);
+    $externalAuthorizationService = $container->get(ExternalAuthorizationService::class);
+    $externalNotificationService = $container->get(ExternalNotificationService::class);
+    $transactionRepository = $container->get(TransactionRepository::class);
+    $userService = $container->get(UserService::class);
+    $walletService = $container->get(WalletService::class);
+    $paymentService = new PaymentService($logger, $externalAuthorizationService, $externalNotificationService, $transactionRepository, $userService, $walletService);
+    return $paymentService;
+});
+$container->set(UserModel::class, function () {
+    $userModel = new UserModel();
+    return $userModel;
+});
+$container->set(UserRepository::class, function (ContainerInterface $container) {
+    $userRepository = new userRepository($container->get(UserModel::class));
+    return $userRepository;
+});
+$container->set(UserService::class, function (ContainerInterface $container) {
+    $userService = new UserService($container->get(UserRepository::class));
     return $userService;
 });
 $container->set(WalletService::class, function () {
     $walletService = new WalletService(new WalletRepository(new WalletModel()));
     return $walletService;
-});
-$container->set(PaymentService::class, function (ContainerInterface $container) {
-    $logger = $container->get("logger");
-    $externalAuthorizationService = $container->get("externalAuthorizationService");
-    $externalNotificationService = $container->get("externalNotificationService");
-    $transactionRepository = new TransactionRepository(new TransactionModel());
-    $userService = $container->get(UserService::class);
-    $walletService = $container->get(WalletService::class);
-    $paymentService = new PaymentService($logger, $externalAuthorizationService, $externalNotificationService, $transactionRepository, $userService, $walletService);
-    return $paymentService;
 });
